75c17ca040c28bfa6b095776ec34e6ba5355d1fd
bodovuse <bodovuse@gmail.com>
2026-06-04 20:34:10 +0100

    branch for ollama development

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 """
-Record short spoken statements from different team members.
-Transcribe the speech with an audio model, such as Vosk.
-Correct the transcript with AI local Ollama.
-Save the results in a CSV dataset.
-Enrich your data with Python.
-Validate the dataset before analysis.
-Produce basic speaking analytics.
+out the box
+    Record short spoken statements from different team members.
+    Transcribe the speech with an audio model, such as Vosk.
+    Correct the transcript with AI local Ollama.
+    Save the results in a CSV dataset.
+
+development
+    Enrich your data with Python.
+    Validate the dataset before analysis.
+    Produce basic speaking analytics.
 """
 #import functions from library
 
