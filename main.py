86c20ef4b97fe4591b1093466d86da262b547a52
bodovuse <bodovuse@gmail.com>
2026-06-07 12:42:28 +0100

    updated todo in main. code ready for merge

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 """
+DONE
 Record short spoken statements from different team members.
 Transcribe the speech with an audio model, such as Vosk.
+TODO
 Correct the transcript with AI local Ollama.
 Save the results in a CSV dataset.
 Enrich your data with Python.
