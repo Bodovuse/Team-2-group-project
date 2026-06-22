4c99c04eb5bd10b232993865d89547b7fd1aa201
bodovuse <bodovuse@gmail.com>
2026-06-22 01:01:29 +0100

    Ollama development done, doing slight adjustments to prompt

--- libraries/ollamaLib.py
+++ libraries/ollamaLib.py
@@ -14,47 +14,46 @@ Before running:
 # output to CSV file
 import csv
 import requests
-#import ollama
-from langchain_ollama import OllamaLLM
-from langchain_experimental.agents.agent_toolkits import create_csv_agent
 
-#CHANGE TO PASSING OLLAMA A FILE AND A PROMPT
+
 MODEL_NAME = "gemma3"
 OLLAMA_URL = "http://localhost:11434/api/generate"
 DATA = "data/transcript.csv"
 
+def askOllama(prompt):
+    response = requests.post(
+        OLLAMA_URL,
+        json={"model": MODEL_NAME, "prompt": prompt,  "stream": False},
+        timeout=360,
+    )
+    response.raise_for_status()
+    return response.json()["response"].strip()
+
 def ollamaMain():
-    llm = OllamaLLM(model = MODEL_NAME, temperature=0.5)
-    agent = create_csv_agent(llm, DATA, verbose = True, allow_dangerous_code = True)
-    agent.handle_parsing_errors = True
-    agent.invoke("Correct this transcript. correct the raw sentence and put them in the column called corrected sentence")
 
-# def askOllama(prompt):
-#     response = requests.post(
-#         OLLAMA_URL,
-#         json={"model": MODEL_NAME, "prompt": prompt,  "stream": False},
-#         timeout=120,
-#     )
-#     response.raise_for_status()
-#     return response.json()["response"].strip()
+    rows = []
 
-# def ollamaMain():
+    print("Ollama initiated\n")
 
-#     #rows = []
+    with open("data/transcript.csv", "r", newline="", encoding="utf-8") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            raw_sentence = row["raw_sentence"]
+            prompt = (f"Correct grammar and wording in the raw text column and return only corrected text: {raw_sentence}")
+            output = askOllama(prompt)
+            print(output)
 
-#     # with open('data/transcript.csv', 'rt') as f:
-#     #     reader = csv.reader(f)
-#     #     for row in reader:
-#     #         rows.append(row)
+            row["corrected_sentence"] = output
+            rows.append(row)
 
-#     #data = "data/transcript.txt"
-#     #data = askOllama(rows)
-#     prompt = (
-#         "Correct this transcript. correct the raw text and add to a column called corrected text:\n"
-#         "$DATA'data/transcript.csv'"
-#     ) 
-#     output = askOllama(prompt)
-#     print("Ollama query sucsessful: ", output)
+    with open("data/transcript.csv", "w", newline="", encoding="utf-8") as f:
+        fieldnames = rows[0].keys()
+
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(rows)
+    print("Ollama done\n")
+    
 
 
     
